6bca064b78c631abad39190f80fb91f62d79110f
Julian Labuschagne <personxx@gmail.com>
2016-02-28 15:47:13 +0200

    Create card from cli.

--- main.py
+++ main.py
@@ -20,25 +20,37 @@ def create_trello_object(cfg):
     )
     return client
 
+def get_trello_list(client, board_name, list_name):
+    for board in client.list_boards():
+        if board_name == board.name.decode('utf-8'):
+            print(board.name.decode('utf-8'))
+            for board_list in board.all_lists():
+                return board_list
+
 if __name__ == "__main__":
     cfg = get_config()
     client = create_trello_object(cfg)
 
+    """Create the parser object and add arguments"""
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--list", action="store_true", help="List Boards")
-    parser.add_argument("-b", "--board", help="Trello Board")
+    parser.add_argument("-c", "--create", action="store_true", help="List Boards")
+    parser.add_argument("-B", "--board", help="Trello Board Name")
+    parser.add_argument("-C", "--card", help="Trello Card Name")
+    parser.add_argument("-L", "--list-name", help="Trello List Name")
     args = parser.parse_args()
 
     if args.list:
         """List of all the Trello Boards"""
         boards = client.list_boards()
 
-        """Print information from Boards"""
+        """Print Board information"""
         for board in boards:
-            print(board.id)
-            print(board.name.decode('utf-8'))
+            print("{}\t{}".format(board.id, board.name.decode('utf-8')))
 
-            for member in board.get_members():
-                print('\t{}'.format(member.full_name.decode('utf-8')))
-    if args.board:
-        print("Chosen board")
+    if args.create:
+        if args.card != "":
+            print("Create a new card")
+            #print(dir(get_trello_list(client, args.board, args.list_name)))
+            new_card = get_trello_list(client, args.board, args.list_name).add_card("New Card Created By Python Script", desc="This card was created by a Python Script called trellocli https://github.com/julian-labuschagne/trellocli")
+            print(new_card)
