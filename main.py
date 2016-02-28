c0f6674a2a2a7a548b6620e0533af3297b4ae058
Julian Labuschagne <personxx@gmail.com>
2016-02-28 21:28:59 +0200

    Create Trello Card function

--- main.py
+++ main.py
@@ -24,8 +24,13 @@ def get_trello_list(client, board_name, list_name):
     for board in client.list_boards():
         if board_name == board.name.decode('utf-8'):
             print(board.name.decode('utf-8'))
+            print(dir(board))
+            print(board.get_labels())
             for board_list in board.all_lists():
                 return board_list
+
+def create_trello_card(board, title, description):
+    board.add_card(title, description)
 
 if __name__ == "__main__":
     cfg = get_config()
@@ -33,15 +38,15 @@ if __name__ == "__main__":
 
     """Create the parser object and add arguments"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("-l", "--list", action="store_true", help="List Boards")
+    parser.add_argument("-v", "--view", action="store_true", help="List Boards")
     parser.add_argument("-c", "--create", action="store_true", help="List Boards")
     parser.add_argument("-B", "--board", help="Trello Board Name")
     parser.add_argument("-C", "--card", help="Trello Card Name")
     parser.add_argument("-L", "--list-name", help="Trello List Name")
     args = parser.parse_args()
 
-    if args.list:
-        """List of all the Trello Boards"""
+    if args.view:
+        """View all the Trello Boards"""
         boards = client.list_boards()
 
         """Print Board information"""
@@ -49,8 +54,9 @@ if __name__ == "__main__":
             print("{}\t{}".format(board.id, board.name.decode('utf-8')))
 
     if args.create:
+        """Create new Trello Objects"""
         if args.card != "":
+            """Create a new Trello card"""
             print("Create a new card")
-            #print(dir(get_trello_list(client, args.board, args.list_name)))
-            new_card = get_trello_list(client, args.board, args.list_name).add_card("New Card Created By Python Script", desc="This card was created by a Python Script called trellocli https://github.com/julian-labuschagne/trellocli")
-            print(new_card)
+            new_card = get_trello_list(client, args.board, args.list_name)
+            create_trello_card(new_card, "New Card Created By Python Script", "This card was created by a Python Script called trellocli https://github.com/julian-labuschagne/trellocli")
